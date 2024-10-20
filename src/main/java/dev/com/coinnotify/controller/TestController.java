7644ce284c956a69ac7f105475c3f226d5f11844
Zua <gimzua0228@gmail.com>
2024-10-20 18:41:38 +0900

    Slack 연결

--- src/main/java/dev/com/coinnotify/controller/TestController.java
+++ src/main/java/dev/com/coinnotify/controller/TestController.java
@@ -1,6 +1,7 @@
-package dev.com.controller;
+package dev.com.coinnotify.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import dev.com.coinnotify.http.SlackHttpClient;
 import dev.com.coinnotify.http.UpbitHttpClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TestController {
     private final UpbitHttpClient upbitHttpClient;
+    private final SlackHttpClient slackHttpClient;
 
     @GetMapping("/api/v1/ticker/{market}")
     public void test(@PathVariable String market) throws JsonProcessingException {
+        System.out.println("Received request for market: " + market);
         upbitHttpClient.getTickerByMarket(market);
+        slackHttpClient.send("hello world");
     }
 }
